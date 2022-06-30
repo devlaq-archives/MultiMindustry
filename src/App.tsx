2ac720f998ec8d99d58f3d9f205e002697d14518
devlaq <devlaq4918@gmail.com>
2022-06-30 21:40:40 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@mui/material';
+import {BottomNavigation, BottomNavigationAction, Box, Button, ButtonGroup, Paper, Tabs} from "@mui/material";
+import {Article, Settings} from "@mui/icons-material";
+
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <Button>
+          {value}
+        </Button>
+        <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+          <BottomNavigation
+              showLabels
+              value={value}
+              onChange={handleChange}
+          >
+            <BottomNavigationAction label="Instances" icon={<Article />} />
+            <BottomNavigationAction label="Settings" icon={<Settings />} />
+          </BottomNavigation>
+        </Box>
+      </Box>
     </div>
   );
 }
